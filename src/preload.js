6578ca9a0b256633262823e040fd3cd3999a396b
jpps <joaopedro-ps@hotmail.com>
2025-05-28 23:38:33 -0300

    reiniciando projeto, IA foi burra nao conseguiu fazer um codigo simples

--- src/preload.js
+++
@@ -1,6 +0,0 @@
-// src/preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  buscarReceita: (nome) => ipcRenderer.invoke('buscar-receita', nome)
-});
