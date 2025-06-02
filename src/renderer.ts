faef3b3c34ae63b4aaff8d8c4c4a13949ec107d1
jpps <joaopedro-ps@hotmail.com>
2025-06-01 22:26:09 -0300

    eventos de adicionar na lista e realizar busca

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,23 @@
 console.log('Hello from Renderer');
+const itens: string[] = [];
 
-const button = document.getElementById('my-button');
-button?.addEventListener('click', () => {
-  alert('Você clicou no botão!');
+
+const itemName = document.getElementById('itemName')  as HTMLInputElement;
+const table =  document.getElementById("myTable") as HTMLTableElement;
+const addButton = document.getElementById('addItem') as HTMLButtonElement;
+const searchButton = document.getElementById('search') as HTMLButtonElement;
+addButton?.addEventListener('click',()=>{  
+  const row = table.insertRow(1);
+  var name = row.insertCell(0);
+  var qtd = row.insertCell(1);
+  name.innerHTML = itemName.value;
+  qtd.innerHTML = "1";
+  itens.push(itemName.value);
+  itemName.value ="";
+  searchButton.style.display="block";
+});
+
+searchButton?.addEventListener('click',()=>{
+  alert("procurar lista de itens");
+  console.log(itens);
 });
